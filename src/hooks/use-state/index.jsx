c71d6a68836aaab802a4d389ef46d07ba3a3f77a
Corina <corina@staykind.io>
2025-04-17 13:14:43 +1000

    added some styling and useState part completed

--- src/hooks/use-state/index.jsx
+++ src/hooks/use-state/index.jsx
@@ -1,7 +1,28 @@
+import {useState} from "react";
+
 export const StateExample = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount((prev) => prev + 1)
+
+    console.log(count);
+  }
+
   return (
-    <div>
-        <p>Count: </p>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh", // optional, for vertical centering
+      
+    }}>
+      <p>Count: {count}</p>
+      <button onClick={increaseCount}>Increase Counter</button>
     </div>
   );
 }
+
+
+// A state is a variable that you can create and change it's value and when it's value is changed, it will automatically trigger a re-render of the page to demonstrate the new value 5:48
